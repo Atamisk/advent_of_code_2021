8069c47bdf4bbb0e022a0956737096b8b32dbde7
Aaron Moore <aaron@atamisk.net>
2021-12-08 16:11:25 -0500

    Day 7 part 2!

--- 07/day_07/src/crabs.rs
+++ 07/day_07/src/crabs.rs
@@ -9,21 +9,62 @@ impl Crabs {
             crab_subs: lst
         }
     }
-    pub fn median(&self) -> usize {
-        let len = self.crab_subs.len();
-        if len % 2 == 0 {
-            self.crab_subs[(len - 1) / 2]
-        }
-        else {
-            let i = (len - 1) / 2;
-            (self.crab_subs[i] + self.crab_subs[i+1]) / 2
-        }
+    pub fn best_pos_1(&self) -> usize {
+        median(self.crab_subs.clone())
     }
-    pub fn fuel_to(&self, pos: usize) -> usize{
+    pub fn best_fuel_2(&self) -> usize {
+        let mut min_fuel = usize::MAX;
+        for pos in 0..{
+            let current_fuel = self.fuel_to_2(pos);
+            if current_fuel > min_fuel {
+                break;
+            }
+            else {
+                min_fuel = current_fuel;
+            }
+        }
+        min_fuel
+    }
+    pub fn fuel_to_1(&self, pos: usize) -> usize{
         self.crab_subs
             .iter()
             .fold(0, |acc, sub| -> usize {
                 acc + ((*sub as isize - pos as isize).abs() as usize)
             })
+    }
+    pub fn fuel_to_2(&self, pos:usize) -> usize {
+        self.crab_subs
+            .iter()
+            .fold(0, |acc, x| -> usize {
+                let err = (pos as isize - *x as isize).abs() as usize;
+                if err != 0 {
+                    acc + (err.pow(2)-((err-1)*err/2))
+                }
+                else {
+                    acc
+                }
+            })
+    }
+}
+fn median(v: Vec<usize>) -> usize {
+    let len = v.len();
+    if len % 2 == 0 {
+        v[(len - 1) / 2]
+    }
+    else {
+        let i = (len - 1) / 2;
+        (v[i] + v[i+1]) / 2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_fuel(){
+        let cr = vec![16,1,2,0,4,2,7,1,2,14];
+        let subs = Crabs::from_list(cr);
+        let med = subs.best_fuel_2();
+        assert_eq!(med, 168);
     }
 }
--- 07/day_07/src/main.rs
+++ 07/day_07/src/main.rs
@@ -6,8 +6,10 @@ fn main() {
     let path = std::env::args().nth(1).expect("No path argument provided.");
     let input: String = read_file::read_to_string(&path).unwrap().trim().to_string();
     let crab_list = parse_input(&input);
-    let sol_1 = solve_1(crab_list);
+    let sol_1 = solve_1(crab_list.clone());
+    let sol_2 = solve_2(crab_list);
     println!("Part 1: {}", sol_1);
+    println!("Part 2: {}", sol_2);
 }
 
 fn parse_input(input: &str) -> Vec<usize>{
@@ -19,8 +21,12 @@ fn parse_input(input: &str) -> Vec<usize>{
 
 fn solve_1(cr: Vec<usize>) -> usize {
     let crabs = Crabs::from_list(cr);
-    let best_pos = crabs.median();
-    crabs.fuel_to(best_pos)
+    let best_pos = crabs.best_pos_1();
+    crabs.fuel_to_1(best_pos)
+}
+fn solve_2(cr: Vec<usize>) -> usize {
+    let crabs = Crabs::from_list(cr);
+    crabs.best_fuel_2()
 }
 
 #[cfg(test)]
@@ -32,5 +38,12 @@ mod tests {
         let cr = parse_input(in_str);
         let sol_1 = solve_1(cr);
         assert_eq!(sol_1,37);
+    }
+    #[test]
+    fn sol_2() {
+        let in_str = "16,1,2,0,4,2,7,1,2,14";
+        let cr = parse_input(in_str);
+        let sol_2 = solve_2(cr);
+        assert_eq!(sol_2,168);
     }
 }
