cdc9601aec086136f40c318f3d7ba6514baf4c02
Aaron Moore <aaron@atamisk.net>
2022-09-02 13:50:55 -0400

    First works on day 10. Not working yet.

--- 10/day_10/src/main.rs
+++ 10/day_10/src/main.rs
@@ -1,3 +1,27 @@
+use read_file::get_lines;
+use std::collections::HashMap;
+
+
 fn main() {
-    println!("Hello, world!");
+   let fname = std::env::args().nth(1).expect("No file name provided");
+   let lines = get_lines(&fname);
+   let map = make_point_map();
+   for line in lines {
+       println!("Hello");
+   }
+}
+
+fn make_point_map() -> HashMap<char, usize> {
+    HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)])
+}
+fn make_mate_map() -> HashMap<char, char> {
+    HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')])
+}
+
+fn get_score(map: HashMap<char, usize>, line: &str) -> usize {
+    let mut stk: Vec<char> = Vec::new();
+    for chr in line.chars() {
+
+    }
+    42
 }
