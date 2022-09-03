290d91752373ff5416b7794fe50bfac96b7a21f3
Aaron Moore <aaron@atamisk.net>
2022-09-02 22:12:53 -0400

    Part 1 day 10 complete

--- 10/day_10/src/main.rs
+++ 10/day_10/src/main.rs
@@ -5,10 +5,17 @@ use std::collections::HashMap;
 fn main() {
    let fname = std::env::args().nth(1).expect("No file name provided");
    let lines = get_lines(&fname);
-   let map = make_point_map();
+   let point_map = make_point_map();
+   let mate_map = make_mate_map();
+
+   let mut total: usize = 0;
    for line in lines {
-       println!("Hello");
+       let line = line.unwrap();
+       if let Some(points) =  get_score(&mate_map, &point_map, &line) {
+           total += points;
+       }
    }
+   println!("{}", total);
 }
 
 fn make_point_map() -> HashMap<char, usize> {
@@ -18,14 +25,18 @@ fn make_mate_map() -> HashMap<char, char> {
     HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')])
 }
 
-fn get_score(mate_map: HashMap<char, char>, point_map: HashMap<char, usize>, line: &str) -> usize {
+fn get_score(mate_map: &HashMap<char, char>, point_map: &HashMap<char, usize>, line: &str) -> Option<usize> {
     let mut stk: Vec<char> = Vec::new();
     for chr in line.chars() {
         if let Some(_) = mate_map.get(&chr){
             stk.push(chr);
         } else if let Some(points) = point_map.get(&chr) {
-            println!("Closing bracket detected.");
+            let mate = stk.pop()?;
+            let correct_chr = *mate_map.get(&mate)?;
+            if chr != correct_chr {
+                return Some(*points);
+            }
         }
     }
-    42
+    None
 }
